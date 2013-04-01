442642d4cdfd28c76b83ab89365c2f792728dc3b
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2013-04-01 04:14:53 +0200

    Also define to_json. This should now work with both json and ActiveModel

--- lib/jsonizer/jsonizer.rb
+++ lib/jsonizer/jsonizer.rb
@@ -2,6 +2,7 @@ class Jsonizer < Module
   def initialize *attributes
     @attributes = attributes
     define_as_json
+    define_to_json
   end
 
   def included mod
@@ -9,16 +10,22 @@ class Jsonizer < Module
     mod.extend(class_methods_module @attributes)
   end
 
-  private
+private
 
   def define_as_json
     attributes = @attributes
-    define_method :as_json do |*_|
+    define_method :as_json do |*|
       hash = {'json_class' => self.class.name}
       attributes.each do |attribute|
         hash[attribute.to_s] = self.send(attribute.to_sym)
       end
       hash
+    end
+  end
+
+  def define_to_json
+    define_method :to_json do |*|
+      as_json.to_json
     end
   end
 
