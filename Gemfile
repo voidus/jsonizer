e81de2c9e26f9490e0ece37bc6421ba7d39345b2
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2013-04-01 04:45:34 +0200

    use coveralls

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in jsonizer.gemspec
 gemspec
+gem 'coveralls', group: :test, require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 unless ENV['autotest']
   require 'simplecov'
+  require 'coveralls'
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
   SimpleCov.start do
     add_filter '/spec/'
   end
