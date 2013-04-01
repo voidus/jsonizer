0bbf8dd2f00706a9c017f329abcb81b82a3748a0
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2013-04-01 02:08:05 +0200

    add existing implementation

---
+++ lib/jsonizer/jsonizer.rb
@@ -0,0 +1,32 @@
+class Jsonizer < Module
+  def initialize *attributes
+    @attributes = attributes
+    define_as_json
+  end
+
+  def included mod
+    mod.extend(class_methods_module @attributes)
+  end
+
+  private
+
+  def define_as_json
+    attributes = @attributes
+    define_method :as_json do |*_|
+      hash = {'json_class' => self.class.name}
+      attributes.each do |attribute|
+        hash[attribute.to_s] = self.send(attribute.to_sym)
+      end
+      hash
+    end
+  end
+
+  def class_methods_module attributes
+    Module.new do
+      define_method :json_create do |values|
+        params = attributes.map {|attribute| values[attribute.to_s]}
+        new(*params)
+      end
+    end
+  end
+end
